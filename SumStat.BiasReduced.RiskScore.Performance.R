7af2e79c5625828ee5aafdb82ad2077307fc3843
David Conti <davidconti@Davids-MacBook-Pro-2.local>
2016-11-10 22:28:03 -0800

    Added the ability to evaluate impact across a range of SE.betas for each value of beta.hat and for each beta.hat at a constant SE.beta. This also allows the investigation of the impact as the p.value changes. 3 plots: by beta.hat by SE.beta by p.value

--- SumStat.BiasReduced.RiskScore.Performance.R
+++ SumStat.BiasReduced.RiskScore.Performance.R
@@ -13,17 +13,21 @@ library(R2jags)
 set.seed(123)
 
 # investigating ranges of shrinkage
-beta.hat <- log(seq(from=1.02, to=1.5, by=0.01))
+beta.hat <- log(seq(from=1.01, to=1.1, by=0.01))
 M <- length(beta.hat)
-se.beta <- rep(0.015, M)
+se.beta <- seq(from=0.01, to=0.02, by=0.001)
+N <- length(se.beta)
+beta.hat <- rep(beta.hat, N)
+se.beta <- rep(se.beta, each=M)
 prec.beta <- 1/(se.beta^2)
+p.value <- 2*(1-pnorm(beta.hat/se.beta))
 
-zeros <- rep(0, M)
+zeros <- rep(0, M*N)
 
 ###### priors on effects
-sigma <- rep(0.05, M)
+sigma <- rep(0.05, M*N)
 sigma2 <- sigma^2
-for(i in 1:M) { print(c(exp(0-1.96*sigma[i]),exp(0+1.96*sigma[i]))) }
+for(i in 1:M*N) { print(c(exp(0-1.96*sigma[i]),exp(0+1.96*sigma[i]))) }
 
 
 ###### Run JAGs hierarchical model
@@ -44,7 +48,7 @@ model.string <-
 }"
 
 jdata <- list(beta.hat=beta.hat,  prec.beta=prec.beta,
-              M=M, sigma2=sigma2,
+              M=M*N, sigma2=sigma2,
               zeros=zeros)
 
 var.s <- c("OR", "beta")
@@ -64,9 +68,38 @@ OR.est <- est[grep("OR", names(est))]
 
 # output results
 # plot of SNP bias reduction
-pdf("SumStat.BiasReduced.RiskScores.Performance.pdf")
-plot(exp(beta.hat), exp(beta.est), typ="l", lwd=2, xlab="MLE estimate", ylab="Biased Reduced Estimate", ylim=c(1,max(exp(beta.hat))), xlim=c(1,max(exp(beta.hat))))
+pdf("SumStat.BiasReduced.RiskScores.SE.Impact.pdf")
+plot(exp(beta.hat[se.beta==se.beta[1]]), exp(beta.est[se.beta==se.beta[1]]), typ="l", lwd=2, xlab="MLE estimate", ylab="Biased Reduced Estimate", ylim=c(1,max(exp(beta.hat))), xlim=c(1,max(exp(beta.hat))))
+l.se.betas <- unique(se.beta)
+index <- 2
+for(s in l.se.betas[l.se.betas!=se.beta[1]]) {
+  lines(exp(beta.hat[se.beta==s]), exp(beta.est[se.beta==s]), col=index)
+  index <- index + 1
+}
+legend(1, max(exp(beta.est)), legend=c(paste("SE=", l.se.betas, sep="")), lty=1, col=1:N, cex=.5)
+dev.off()
+pdf("SumStat.BiasReduced.RiskScores.Beta.Impact.pdf")
+plot(se.beta[beta.hat==beta.hat[1]], exp(beta.est[beta.hat==beta.hat[1]]), typ="l", lwd=2, xlab="SE of the MLE estimate", ylab="Biased Reduced Estimate", ylim=c(1,max(exp(beta.hat))))
 abline(a=0, b=1, lty=2)
+l.betas <- unique(beta.hat)
+index <- 2
+for(s in l.betas[l.betas!=beta.hat[1]]) {
+  lines(se.beta[beta.hat==s], exp(beta.est[beta.hat==s]), col=index)
+  index <- index + 1
+}
+legend(se.beta[1], max(exp(beta.est)), legend=c(paste("OR.hat=", round(exp(l.betas),3), sep="")), lty=1, col=1:M, cex=.5)
+dev.off()
+
+
+pdf("SumStat.BiasReduced.RiskScores.P.Value.Impact.pdf")
+l.betas <- unique(beta.hat)
+plot(-log(p.value), exp(beta.est), pch=16, col=match(beta.hat, l.betas), xlab="-log(P.Value) of the MLE estimate", ylab="Biased Reduced Estimate", ylim=c(1,max(exp(beta.hat))))
+index <- 1
+for(s in l.betas) {
+  abline(h=exp(s), col=index, lty=2, lwd=2)
+  index <- index + 1
+}
+legend(se.beta[1], max(exp(beta.est)), legend=c(paste("OR.hat=", round(exp(l.betas),3), sep="")), lty=1, col=1:M, cex=.5)
 dev.off()
 
 r <- summary(model.fit)
